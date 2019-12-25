ab7421147fb0bf4a1a2fa33b5bf2f914825dc9c6
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2019-12-25 17:21:54 +0900

    save

---
+++ app/helpers/registrationacc_helper.rb
@@ -0,0 +1,2 @@
+module RegistrationaccHelper
+end
---
+++ test/controllers/registrationacc_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RegistrationaccControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
