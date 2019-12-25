f31283543b40cce6dd36bf2c9b752e84d0e5f1bc
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2019-12-25 19:22:12 +0900

    スタッシュから復活

---
+++ app/controllers/registrationaccs_controller.rb
@@ -0,0 +1,4 @@
+class RegistrationaccsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/registrationaccs_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RegistrationaccsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
