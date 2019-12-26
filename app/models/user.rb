c45a3f62b6020b2bb69c654d32ba72c96e7fe31e
gratelyas <gratel.yas@gmail.com>
2019-12-26 21:01:14 +0900

    commit-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday ,presence: true
+  
   has_one :address
   has_one :card
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
   factory :user do
     nickname              {"abe"}
     email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    password              {"0000000"}
+    password_confirmation {"0000000"}
     birthday              {"1993-05-02"}
     last_name             {"瀬戸"}
     first_name            {"小菜"}
     last_name_kana        {"セト"}
-    first_name            {"ヤス"}
+    first_name_kana       {"ヤス"}
   end
 
 end
