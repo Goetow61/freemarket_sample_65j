3b22bc70f1c92f510ca9efe2196cac4acd7bd0cf
yokoyoko3 <57942559+yokoyoko3@users.noreply.github.com>
2019-12-28 15:23:00 +0900

    snsコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  kanji = /\A[一-龥]+\z/
+  kana = /\A([ァ-ン]|ー)+\z/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
-  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday ,presence: true
+ validates :nickname, presence: true, length: { maximum: 15 }
+  validates :first_name, presence: true, length: { maximum: 15 }, format: { with: kanji }
+  validates :last_name, presence: true, length: { maximum: 15 }, format: { with: kanji }
+  validates :first_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
+  validates :last_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
+  validates :birthday ,presence: true
   has_one :address
   has_one :card
   has_many :sns_credential
---
+++ spec/factories/addresses.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :address do
+    last_name                   {"森田"}
+    first_name                  {"一義"}
+    last_name_kana              {"モリタ"}
+    first_name_kana             {"カズヨシ"}
+    postal_code                 {"117-1147"}
+    prefecture                  {"13"}
+    city                        {"渋谷区"}
+    address                     {"原宿"}
+  end
+
+end
+
