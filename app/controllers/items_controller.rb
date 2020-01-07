204338358361c3a8c82f9eceb724e9a3349ca0a2
gratelyas <gratel.yas@gmail.com>
2020-01-07 16:39:02 +0900

    commit-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,8 +55,11 @@ class ItemsController < ApplicationController
 
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   
