2dcb1c3f68286a3b578ef602bd434e1f41761e63
gratelyas <gratel.yas@gmail.com>
2020-01-13 19:17:48 +0900

    commit-destroy-ok

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
   mount_uploader :src, ImageUploader
-  belongs_to :item, dependent: :destroy
+  belongs_to :item
   validates :src ,presence: true
   default_scope -> { order(created_at: :desc) }
 end
