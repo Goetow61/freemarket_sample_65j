71e3fbb1fb9c21e36ef3cb4179f22c3386e4021f
techexpert65-19894 <u_ku_su_tu_nu@hotmail.co.jp>
2020-01-18 10:07:40 +0900

    商品一覧追加(メンズ、家電、おもちゃ)_import用CSV

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,9 @@ class TopController < ApplicationController
   
   def index
     @ladies = Item.where(category: "レディース").take(10)
+    @mens = Item.where(category: "メンズ").take(10)
+    @homeAppliances = Item.where(category: "家電・スマホ・カメラ").take(10)
+    @toys = Item.where(category: "おもちゃ・ホビー・グッズ").take(10)
   end
 
   def new
